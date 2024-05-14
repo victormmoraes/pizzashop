3779b2335ebcf288fb78ec18375516e395dcf796
Victor Medrado <victormedrado49@gmail.com>
2024-05-14 13:11:19 -0300

    Estrutura visual - Criando gráfico de pizza de produtos populares usando Rechart

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -53,6 +53,8 @@ export function RevenueChart() {
                 })
               }
             />
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
             <Line
               type="linear"
               strokeWidth={2}
