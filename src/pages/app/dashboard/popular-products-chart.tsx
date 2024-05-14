e12f117c95f10939f45568a1eff59e39184eca0d
Victor Medrado <victormedrado49@gmail.com>
2024-05-14 18:11:46 -0300

    Estrutura visual - Página 404

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,5 +1,5 @@
 import { BarChart } from 'lucide-react'
-import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
