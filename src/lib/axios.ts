03903e76a3189fbe9aefdcc3261ab1414ba148f9
Victor Medrado <victormedrado49@gmail.com>
2024-05-15 15:50:41 -0300

    Autenticação do Usuário

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-import { env } from '@/env'
+// import { env } from '@/env'
 
 export const api = axios.create({
-  baseURL: env.VITE_API_URL,
+  baseURL: 'http://localhost:3333',
 })
